500a72e86b290b42987910d67e7df7c5c35e6fc0
SquarePear <16364318+SquarePear@users.noreply.github.com>
2023-03-23 11:09:16 -0400

    Improve player controller
    Signed-off-by: SquarePear <16364318+SquarePear@users.noreply.github.com>

--- project/ship/ship.gd
+++ project/ship/ship.gd
@@ -1,16 +1,44 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
+const SPEED = 15.0
+const DODGE_SPEED = 100.0
+const DODGE_SLOWDOWN = 0.5
+
+var can_dodge := true
+var dodging := false
 
 @onready var camera_mount := $CameraMount
+@onready var dodge_cooldown_timer := $DodgeCooldownTimer
 
 
 func _physics_process(_delta):
 	var direction = _get_camera_oriented_input()
+	var just_dodged = Input.is_action_just_pressed("dodge")
+
+	if can_dodge && direction && just_dodged && !dodging:
+		var normalized_direction = direction.normalized()
+
+		velocity.x = normalized_direction.x * DODGE_SPEED
+		velocity.z = normalized_direction.z * DODGE_SPEED
+
+		dodging = true
+		can_dodge = false
+
+		dodge_cooldown_timer.start()
+
+	if dodging:
+		velocity = velocity.move_toward(Vector3.ZERO, SPEED * DODGE_SLOWDOWN)
+
+		move_and_slide()
+
+		if velocity == Vector3.ZERO:
+			dodging = false
+
+		return
 
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = move_toward(velocity.x, direction.x * SPEED, SPEED)
+		velocity.z = move_toward(velocity.z, direction.z * SPEED, SPEED)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
@@ -19,7 +47,7 @@ func _physics_process(_delta):
 		Input.get_action_strength("move_up") - Input.get_action_strength("move_down")
 	)
 
-	if vertical_input:
+	if vertical_input && !just_dodged:
 		velocity.y = move_toward(velocity.y, vertical_input * SPEED, SPEED)
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
@@ -38,3 +66,7 @@ func _get_camera_oriented_input() -> Vector3:
 	input = camera_mount.global_transform.basis * input
 	input.y = 0.0
 	return input
+
+
+func _on_dodge_cooldown_timer_timeout():
+	can_dodge = true
