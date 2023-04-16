a90f7756fc00e484ad595c6bd9ea8c115e0d81b8
Jennifer Coy <jennifer.coy@bsu.edu>
2023-04-16 18:16:34 -0400

    Added new turret, new particles, new missle model

--- project/turret/turret.gd
+++ project/turret/turret.gd
@@ -2,12 +2,19 @@ extends StaticBody3D
 
 @export var target: Node3D
 
-@onready var barrel = $Barrel
+@onready var barrel = $Node3D/Node3D2
 @onready var animation_player = $AnimationPlayer
 @onready var projectile_scene = preload("res://projectile/projectile.tscn")
+@onready var base = $Node3D
 
+func _ready():
+	target = get_node("/root/World1/Ship")
 
 func _process(_delta):
+	base.look_at(target.global_transform.origin, Vector3.UP)
+	base.rotation.x = 0
+	base.rotation.z = 0
+	
 	barrel.look_at(target.global_transform.origin, Vector3.UP)
 
 
