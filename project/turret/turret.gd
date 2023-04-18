9ff882fa48f9df374b5153bdc9268b5ec2044df3
SquarePear <16364318+SquarePear@users.noreply.github.com>
2023-04-18 11:42:56 -0400

    Add range and friendly fire detectors to turrets

--- project/turret/turret.gd
+++ project/turret/turret.gd
@@ -1,29 +1,70 @@
 extends StaticBody3D
 
-@export var target: Node3D
+var target_in_range := false
 
-@onready var barrel = $Node3D/Node3D2
-@onready var animation_player = $AnimationPlayer
-@onready var projectile_scene = preload("res://projectile/projectile.tscn")
-@onready var base = $Node3D
-
-
-func _ready():
-	target = get_node("/root/World1/Ship")
+@onready var barrel := $Node3D/Node3D2
+@onready var animation_player := $AnimationPlayer
+@onready var projectile_scene := preload("res://projectile/projectile.tscn")
+@onready var base := $Node3D
+@onready var target: PhysicsBody3D = get_node("/root/World1/Ship")
+@onready var friendly_fire_detector: RayCast3D = %FriendlyFireDetector
+@onready var shoot_timer: Timer = $ShootTimer
 
 
 func _process(_delta):
+	if not target_in_range:
+		return
+
+	look_at_target()
+
+
+func look_at_target():
 	base.look_at(target.global_transform.origin, Vector3.UP)
 	base.rotation.x = 0
 	base.rotation.z = 0
 	barrel.look_at(target.global_transform.origin, Vector3.UP)
 
 
+func _will_friendly_fire():
+	if !friendly_fire_detector.is_colliding():
+		return false
+
+	var hit_target := friendly_fire_detector.get_collider()
+
+	return hit_target.is_in_group("destroyable")
+
+
 func shoot():
+	animation_player.play("shoot")
+
 	var projectile = projectile_scene.instantiate()
 	get_tree().get_root().add_child(projectile)
 	projectile.global_transform = barrel.global_transform
 
 
 func _on_shoot_timer_timeout():
-	animation_player.play("shoot")
+	if not target_in_range:
+		return
+
+	shoot_timer.start()
+
+	if _will_friendly_fire():
+		return
+
+	shoot()
+
+
+func _on_targeting_area_body_entered(body: Node3D):
+	if body != target:
+		return
+
+	target_in_range = true
+	shoot_timer.start()
+
+
+func _on_targeting_area_body_exited(body: Node3D):
+	if body != target:
+		return
+
+	target_in_range = false
+	shoot_timer.stop()
